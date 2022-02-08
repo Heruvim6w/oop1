995ee895347d67e5de7dbfb7ceea63c6bb5c3f66
Salkov_a <heruvim.6w@gmail.com>
2022-02-08 21:01:55 +0300

    Добавили тесты (надо починить)

--- Classes/Product.php
+++ Classes/Product.php
@@ -17,7 +17,8 @@ class Product
     /**
      * @return array
      */
-    public function getProduct(): array{
+    public function getProduct(): array
+    {
         $products = [
             'Name' => $this->_name,
             'Price' => $this->_price
@@ -34,12 +35,81 @@ class Product
     {
         foreach ($products as $product) {
             $result = array_search($name, $product);
-            if ($result) {
-                return $product;
-            }
-            else {
-                throw new \Exception('Продукт не найден');
-            }
+
+            var_dump($name, $product, $result);
+            echo "\n------------------------------";
+
+//            if ($result) {
+//                return $product;
+//            }
+//            else {
+//                throw new \Exception('Продукт не найден');
+//            }
         }
     }
+
+    public function f()
+    {
+        return [
+            [
+                // продукты
+                [[
+                    'Name' => "продукт1",
+                    'Price' => 0.0
+                ],[
+                    'Name' => "продукт2",
+                    'Price' => 0.2
+                ],[
+                    'Name' => "продукт3",
+                    'Price' => 0.1
+                ],],
+                // какой продукт ищем
+                "продукт2",
+                // Результат
+                [
+                    'Name' => "продукт2",
+                    'Price' => 0.0
+                ],
+            ]
+        ];
+    }
+
+    public function magic()
+    {
+        // dataprovider
+        $data = $this->f();
+
+        foreach ($data as $key => [$products, $product, $result]) {
+            $this->searchByNameTest($products, $product, $result);
+        }
+    }
+
+    /**
+     * @dataprovider f
+     */
+    public function searchByNameTest($products, $product, $result)
+    {
+        $this->assertEquals(
+            // актуальные данные
+            $this->searchByName($products, $product),
+            // ожидаемый результат
+            $result
+        );
+    }
+
+    private function assertEquals($actual, $expected)
+    {
+        if ($actual === $expected) {
+            return;
+        }
+
+        throw new Exception(sprintf(
+            "Все плохо!!! актуальный результат : %s, ожидаемый результат : %s",
+            implode("|", $actual),
+            implode("|", $expected)
+        ));
+    }
 }
+
+$p = new Product();
+$p->magic();
